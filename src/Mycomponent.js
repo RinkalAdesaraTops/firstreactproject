6aa2963b094e62949753753207d2469bfc937eba
RinkalADesara <rinkaladesara.tops@gmail.com>
2024-07-08 15:43:39 +0530

    Done

---
+++ src/Mycomponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Mycomponent = () => {
+    console.log('component created by me.');
+  return (
+    <div>
+      <h3>My First component</h3>
+    </div>
+  )
+}
+
+export default Mycomponent
